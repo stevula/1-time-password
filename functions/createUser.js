be5e354aa978b6cdcabd93366761c40c8e77c34b
stevula <brodericksteven@gmail.com>
2018-06-16 17:55:49 -0700

    Set up createUser to create user from phone

--- functions/createUser.js
+++ functions/createUser.js
@@ -1,3 +1,13 @@
+const admin = require('firebase-admin');
+
 module.exports = function createUser(req, res) {
-  res.send(req.body);
+  if (!req.body.phone) {
+    return res.status(422).send({ error: 'Missing phone number' });
+  }
+
+  const phone = String(req.body.phone).replace(/\D/g, '');
+
+  return admin.auth().createUser({ uid: phone })
+    .then(user => res.send(user))
+    .catch(err => res.status(422).send({ error: err }));
 };
